9f2b0adb7cc6c5f029e669d397ba19a719e06c76
Matheus Baron <matheusbaron10@gmail.com>
2024-01-19 11:59:44 -0300

    feat: create order details dialog

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,16 +1,24 @@
 import { ArrowRight, Search, X } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
+
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant='outline' size='xs' >
-          <Search className="h-3 w-3" />
-          <span className="sr-only" >Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild >
+            <Button variant='outline' size='xs' >
+              <Search className="h-3 w-3" />
+              <span className="sr-only" >Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-sm font-medium" >
         s21sd21fsd21fsd1
@@ -19,7 +27,7 @@ export function OrderTableRow() {
         há 15 minutos
       </TableCell>
       <TableCell>
-        <div className="flex items-tencer gap-2" >
+        <div className="flex items-center gap-2" >
           <span className="h-2 w-2 rounded bg-slate-400" />
           <span className="font-medium text-muted-foreground" >Pendente</span>
         </div>
