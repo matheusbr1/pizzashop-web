837c70dbfc6c7fb904b2633bb2c9aeb6940fad40
Matheus Baron <matheusbaron10@gmail.com>
2024-01-27 19:06:49 -0300

    feat: integrate orders with API

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,12 +1,25 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { ArrowRight, Search, X } from "lucide-react";
 
+import { OrderStatus } from "@/components/order-status";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
 
 import { OrderDetails } from "./order-details";
 
-export function OrderTableRow() {
+interface OrderTableRowProps {
+  order: {
+    orderId: string;
+    createdAt: Date;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+  }
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -21,22 +34,25 @@ export function OrderTableRow() {
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-sm font-medium" >
-        s21sd21fsd21fsd1
+        {order.orderId}
       </TableCell>
       <TableCell className="text-muted-foreground" >
-        há 15 minutos
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true
+        })}
       </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2" >
-          <span className="h-2 w-2 rounded bg-slate-400" />
-          <span className="font-medium text-muted-foreground" >Pendente</span>
-        </div>
+        <OrderStatus status={order.status} />
       </TableCell>
       <TableCell className="font-medium" >
-        Matheus Baron
+        {order.customerName}
       </TableCell>
       <TableCell className="font-medium" >
-        R$ 149,90
+        {order.total.toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL'
+        })}
       </TableCell>
       <TableCell>
         <Button variant='outline' size='xs' >
