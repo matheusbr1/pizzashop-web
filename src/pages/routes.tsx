b18e4bcff49afbbb12853e8579ee1e9b0323a9da
Matheus Baron <matheusbaron10@gmail.com>
2024-02-11 13:45:25 -0300

    feat: create Error page

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -7,26 +7,28 @@ import { Dashboard } from './app/dashboard/dashboard'
 import { Orders } from './app/orders/orders'
 import { SignIn } from './auth/sign-in'
 import { SignUp } from './auth/sign-up'
+import { Error } from './error'
 
 export const router = createBrowserRouter([
   {
-    path: '/', element: <AppLayout />,
-    errorElement: <NotFound />,
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
     ]
   },
   {
-    path: '/', element: <AuthLayout />,
+    path: '/',
+    element: <AuthLayout />,
     children: [
-      { path: '/sign-in', element: <SignIn /> }
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
     ]
   },
   {
-    path: '/', element: <AuthLayout />,
-    children: [
-      { path: '/sign-up', element: <SignUp /> }
-    ]
+    path: '*',
+    element: <NotFound />,
   },
 ])
