c22891af2ecec565d85f9803c3a77152e0ac9e0e
Matheus Baron <matheusbaron10@gmail.com>
2024-02-11 12:50:59 -0300

    feat: create loading state with spin into revenue and  products charts

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { subDays } from 'date-fns'
+import { Loader2 } from 'lucide-react';
 import { useMemo, useState } from 'react';
 import { DateRange } from 'react-day-picker';
 import { CartesianGrid, Label, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
@@ -46,7 +47,7 @@ export function RevenueChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {dailyRevenueInPeriod && (
+        {dailyRevenueInPeriod ? (
           <ResponsiveContainer width='100%' height={240} >
             <LineChart style={{ fontSize: 12 }} data={chartData} >
               <YAxis
@@ -79,6 +80,10 @@ export function RevenueChart() {
               />
             </LineChart>
           </ResponsiveContainer>
+        ) : (
+          <div className='flex h-[240px] w-full items-center justify-center' >
+            <Loader2 className='h-8 w-8 animate-spin text-muted-foreground' />
+          </div>
         )}
       </CardContent>
     </Card>
