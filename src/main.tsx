81ec8fda1b36aadd53872c7624e5df1a9d0d6bb4
Matheus Baron <matheusbaron10@gmail.com>
2024-02-14 11:39:14 -0300

    config: setup MSW

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { enableMSW } from './api/mocks/index.ts'
 import { App } from './app.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+enableMSW().then(() => {
+  return ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
